9b107780d15ad8c38d8f7d37bdebfcb771557a41
Jordan Glean Veitia <jordanglean@outlook.com>
2024-01-04 06:01:07 +0000

    refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,7 @@
 // Models
 const Tour = require('../models/tourModel');
+// Utils
+const APIFeatures = require('../utils/apiFeatures');
 
 // Alias Middleware
 exports.aliasTopTours = async (req, res, next) => {
@@ -8,66 +10,6 @@ exports.aliasTopTours = async (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  // Filter
-  filter() {
-    // eslint-disable-next-line node/no-unsupported-features/es-syntax
-    const queryObj = { ...this.queryString };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advance Filtering -> MongoDB
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`,
-    );
-
-    this.query = this.query.find(JSON.parse(queryString));
-
-    return this;
-  }
-
-  // Sorting
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-    return this;
-  }
-
-  // 3) Field Limiting
-  limiting() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields); // called projecting
-    } else {
-      this.query = this.query.select('-__v'); // exclude it
-    }
-    return this;
-  }
-
-  // Pagination
-  pagination() {
-    // 4) Pagination
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-}
 
 // GET All Tours Route
 exports.getAllTours = async (req, res) => {
